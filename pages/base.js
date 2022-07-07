058412c4550738b59380c2595d4d119611d4b454
nickblaq <nickookoro200@gmail.com>
2022-07-07 09:27:02 +0100

    patch 0.9

--- pages/base.js
+++ pages/base.js
@@ -1,5 +1,5 @@
-import Airtable from 'airtable'
+// import Airtable from 'airtable'
 
-const base = new Airtable({apiKey: 'keyQbFA8IznLJs1If'}).base('appEYBJbEFPSTTPFU')
+// const base = new Airtable({apiKey: 'keyQbFA8IznLJs1If'}).base('appEYBJbEFPSTTPFU')
 
-export default base;
+// export default base;
